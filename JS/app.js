6c9986d6a63eb54afe5ab8aeedd07e50b987f182
elaelele <35705673+elaelele@users.noreply.github.com>
2018-10-25 21:07:58 +0100

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,12 @@
-alert('Hello World!');
+// alert('Hello World!');
+function isWindy(speed){
+	if (speed > 5) {
+		alert('It is windy!');
+	} else {
+		alert('It is not windy');
+	}
+}
+
+isWindy(10);
+isWindy(4);
+isWindy(888);
